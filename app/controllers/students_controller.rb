1c018fa71610f5368b099d866bc165651c95aee1
Chris Gullickson <cgullickson5@gmail.com>
2018-02-05 18:06:27 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -17,7 +17,7 @@ class StudentsController < ApplicationController
       @student.active = false
     end
     @student.save
-    redirect_to students_path(student)
+    redirect_to students_path(@student)
   end
 
   private
